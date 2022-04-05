92c7c9c74b002981da38ddbc7acc8c9db7e0e67e
Kahena Mansano <kahenamansanoalmeida@gmail.com>
2022-04-05 18:12:09 -0400

    Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,64 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import moment from 'moment'
+import './App.css'
 
 function App() {
-  const [pageCode, setPage] = useState('2359')
-  const [color, setColor] = useState('#6BEFA3')
-  const [name, setName] = useState('Mega-Sena')
-  const [results, setResults] = useState({
-    id: '2359',
-    loteria: 0,
-    numeros: Array(6),
-    data: '2022-04-02T01:01:36.515Z'
-  })
-  const [newDate, setNewDate] = useState()
+  const [pageCode, setPage] = useState<string | null>(null)
+  const [color, setColor] = useState<string | null>(null)
+  const [name, setName] = useState<string | null>(null)
+  const [results, setResults] = useState<object | null>(null)
+  const [newDate, setNewDate] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
 
-  // if (pageCode === '2359') {
-  //   setColor('#6BEFA3')
-  //   setName('Mega-Sena')
-  // }
-
-  if (pageCode === '5534') {
-    setColor('#8666EF')
-    setName('Quina')
+  if (name === 'MEGA-SENA' && pageCode !== '2359') {
+    setColor('#6BEFA3')
+    setPage('2359')
   }
 
-  function update(event) {
-    const select = event.target.value
-    setPage(select)
+  if (name === 'QUINA' && pageCode !== '5534') {
+    setColor('#8666EF')
+    setPage('5534')
+  }
+
+  if (name === 'LOTOFACIL' && pageCode !== '2200') {
+    setColor('#DD7AC6')
+    setPage('2200')
+  }
+
+  if (name === 'LOTOMANIA' && pageCode !== '2167') {
+    setColor('#FFAB64')
+    setPage('2167')
+  }
+
+  if (name === 'TIMEMANIA' && pageCode !== '1622') {
+    setColor('#5AAD7D')
+    setPage('1622')
+  }
+
+  if (name === 'DIA DE SORTE' && pageCode !== '440') {
+    setColor('#BFAF83')
+    setPage('440')
+    console.log('setou cor e código da página')
+  }
+
+  function update(e: React.ChangeEvent<HTMLSelectElement>) {
+    const select = e.target.value
+    setName(select)
+    console.log(select, 'setou no nome')
   }
 
   useEffect(() => {
+    if (!name) return
+
     console.log('Entrou no Request')
+
     async function searchData() {
       const request = await axios.get(
-        // `https://brainn-api-loterias.herokuapp.com/api/v1/concursos/${pageCode}`
-        'https://brainn-api-loterias.herokuapp.com/api/v1/concursos/2359'
+        `https://brainn-api-loterias.herokuapp.com/api/v1/concursos/${pageCode}`
       )
       setResults(request.data)
+      setLoading(false)
       console.log(request.data, 'Request feito')
 
       return request
@@ -45,55 +68,60 @@ function App() {
   }, [pageCode])
 
   useEffect(() => {
+    if (!results) return
     function isoFormatDMY() {
       let date = results.data
       let dateRange = moment.utc(date).format('YYYY-MM-DD')
-      alert(new Date(dateRange))
+      setNewDate(dateRange)
     }
 
-    // const date = results.data
-    // function isoFormatDMY(date) {
-    //   function pad(n) {
-    //     return (n < 10 ? '0' : '') + n
-    //   }
-    //   return (
-    //     pad(date.getUTCDate()) +
-    //     '/' +
-    //     pad(date.getUTCMonth() + 1) +
-    //     '/' +
-    //     date.getUTCFullYear()
-    //   )
-    // }
-    // console.log(isoFormatDMY(), 'conversão data')
     isoFormatDMY()
   }, [results])
 
-  var s = '2014-11-03T19:38:34.203Z'
-  var date = parseISOString(s)
-
-  console.log(isoFormatDMY(date))
-
   return (
-    <div style={{ backgroundColor: color, padding: '0', margin: '0' }}>
-      <select id="page" onChange={update}>
-        <option defaultValue=""> </option>
-        <option value="2359">Mega-Sena</option>
-        <option value="5534">Quina</option>
-        <option value="2200">Loto Fácil</option>
-        <option value="2167">Loto Mania</option>
-        <option value="1622">Time Mania</option>
-        <option value="440">Dia de sorte</option>
+    <div className="Page">
+      <select id="page" onChange={update} className="Input">
+        <option defaultValue="">Selecione o concurso:</option>
+        <option value="MEGA-SENA">MEGA-SENA</option>
+        <option value="QUINA">QUINA</option>
+        <option value="LOTOFACIL">LOTOFACIL</option>
+        <option value="LOTOMANIA">LOTOMANIA</option>
+        <option value="TIMEMANIA">TIMEMANIA</option>
+        <option value="DIA DE SORTE">DIA DE SORTE</option>
       </select>
-      <h1>{name}</h1>
 
-      <div>
-        {results.numeros.map(number => {
-          return <div>{number}</div>
-        })}
-      </div>
-      <h2>Concurso</h2>
-      <p>{results.id}</p>
-      <p>{results.data}</p>
+      {!loading ? (
+        <div>
+          <img src="./Logo_Sena.png" />
+          <h1>{name}</h1>
+
+          <div>
+            {results.numeros.map(number => {
+              return <div key={number}>{number}</div>
+            })}
+          </div>
+
+          <h2>Concurso</h2>
+          <p>
+            {results.id} - {newDate}
+          </p>
+        </div>
+      ) : (
+        <div class="lds-default">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
     </div>
   )
 }
